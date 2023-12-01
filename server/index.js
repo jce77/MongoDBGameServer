d90417218f443672267f0aa373a4f9ba2e43c7fa
jce77 <91095816+jce77@users.noreply.github.com>
2023-12-01 06:58:15 -0330

    Update 2
    -Added server-side functionality for adding a player's score and deleting the lowest from the collection.

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const app = express();
 const port = 3000;
@@ -16,10 +17,47 @@ const highscoreSchema = new mongoose.Schema({
 // Create a model for the Highscores collection
 const HighscoreModel = mongoose.model('Highscore', highscoreSchema);
 
+// Middleware to parse JSON requests
+app.use(bodyParser.json());
+
 app.get('/highscores', async (req, res) => {
     try {
       const highscores = await HighscoreModel.find({}, { _id: 0, __v: 0 }); // Exclude _id and __v fields
       res.json({ highscores });
+    } catch (error) {
+      console.error(error);
+      res.status(500).send('Internal Server Error');
+    }
+  });
+
+
+// Express route to add a new highscore
+app.post('/addhighscore', async (req, res) => {
+    try {
+      const { name, highscore } = req.body;
+  
+      // Check if max highscores limit is reached
+      const currentHighscoresCount = await HighscoreModel.countDocuments();
+      const maxHighscores = 10;
+  
+      if (currentHighscoresCount < maxHighscores) 
+      {
+        // If there's room, add the highscore directly
+        await HighscoreModel.create({ name, highscore });
+      } 
+      else 
+      {
+        // If max highscores is reached, find the lowest highscore
+        const lowestHighscore = await HighscoreModel.findOne().sort({ highscore: 1 });
+  
+        // If the new highscore is higher, replace the lowest one
+        if (highscore > lowestHighscore.highscore) {
+          await HighscoreModel.findOneAndDelete({ _id: lowestHighscore._id });
+          await HighscoreModel.create({ name, highscore });
+        }
+      }
+  
+      res.status(200).send('Highscore added successfully');
     } catch (error) {
       console.error(error);
       res.status(500).send('Internal Server Error');
